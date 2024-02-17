18a14f4223a2569ca06f4308a76a658178405732
naraku0211 <kentivanunabia0211@gmail.com>
2024-02-17 10:20:12 +0800

    v1.0

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string? Name {  get; set; }
+        [Required(ErrorMessage = "Please enter your email address.")]
+        public string? Email { get; set; }
+        [Required(ErrorMessage = "Please enter your phone/cell #.")]
+        public string? Phone {  get; set; }
+        [Required(ErrorMessage = "Specify whether you will attend the party.")]
+        public bool? WillAttend { get; set; }   
+
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace PartyInvites.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> _response = new ();
+
+        public static IEnumerable<GuestResponse> Responses => _response;
+
+        public static void AddResponse(GuestResponse response)
+        {
+            Console.WriteLine(response);
+            _response.Add(response);
+        }
+    }
+}
